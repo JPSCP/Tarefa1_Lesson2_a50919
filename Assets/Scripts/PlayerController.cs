7fba2503b8fdeb760540b295984029f5388dee1d
JPSCP <a50919@alunos.ipb.pt>
2026-03-06 12:47:23 +0000

    Lesson 2.2. 1-4  - Food Flight
    1. Make the projectile fly forwards
    2. Make the projectile into a prefab
    3. Test for spacebar press
    4. Launch projectile on spacebar press

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.WSA;
 
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public GameObject projectilePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,6 +26,12 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        // Launch a projectile from the player
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
